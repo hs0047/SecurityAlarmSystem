c7549f8b641aeb35da73ea559c5563b41b4ee115
Himanshu Shukla <91239771+hs0047@users.noreply.github.com>
2024-03-24 16:27:23 +0530

    ok

--- Backend/src/config/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    mongodbUri: process.env.MONGODB_URI,
-    cloudAmqpUrl: process.env.CLOUDAMQP_URL,
-    dataGenerationIntervalMs: parseInt(process.env.DATA_GENERATION_INTERVAL_MS, 10) || 5000,
-    continiousSourceAnomaliTime: parseInt(process.env.CONTINIOUS_SOURCE_ANOMALI_TIME, 10) || 7200000,
-};
